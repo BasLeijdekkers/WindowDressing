c6552224bedf7f0b04121b3fb46c625c524e4af8
Maarten Hazewinkel <maarten.hazewinkel@gmail.com>
2011-06-23 09:45:36 +0200

    Fix incompatibility with current IDEA 10.5 release version

--- src/net/intellij/window/WindowAction.java
+++ src/net/intellij/window/WindowAction.java
@@ -16,6 +16,7 @@
 package net.intellij.window;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
@@ -86,7 +87,7 @@ public class WindowAction extends ToggleAction implements DumbAware {
 
     public boolean isSelected(AnActionEvent e) {
         // show check mark for active and visible project frame
-        final Project project = e.getProject();
+        final Project project = e.getData(PlatformDataKeys.PROJECT);
         final String text = getTemplatePresentation().getText();
         return text.equals(project.getName());
     }
