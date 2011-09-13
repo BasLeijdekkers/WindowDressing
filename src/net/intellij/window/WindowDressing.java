47f9c4233b16fb8e49f3952f7244523b6d3b3fa9
Bas Leijdekkers <basleijdekkers@gmail.com>
2011-09-13 10:56:47 +0200

    Restore project window position storing and restoring now that plugin is integrated in IDEA core

--- src/net/intellij/window/WindowDressing.java
+++ src/net/intellij/window/WindowDressing.java
@@ -22,7 +22,20 @@ import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 
-public class WindowDressing extends AbstractProjectComponent {
+@State(name="WindowDressing", roamingType = RoamingType.DISABLED,
+        storages = {@Storage(id="other", file = "$WORKSPACE_FILE$")})
+public class WindowDressing extends AbstractProjectComponent implements
+        PersistentStateComponent<WindowDressing.WindowState> {
+
+    public static class WindowState {
+        public int x = -1;
+        public int y = -1;
+        public int width = -1;
+        public int height = -1;
+        public int extendedState = -1;
+    }
+
+    private WindowState windowState;
 
     public WindowDressing(@NotNull Project project) {
         super(project);
@@ -31,6 +44,19 @@ public class WindowDressing extends AbstractProjectComponent {
     public void projectOpened() {
         final WindowActionGroup windowActionGroup = getWindowActionGroup();
         windowActionGroup.addProject(myProject.getName());
+        if (windowState == null || windowState.x == -1 || windowState.y == -1 ||
+                windowState.width == -1 || windowState.height == -1) {
+            return;
+        }
+        final Frame projectFrame = WindowAction.findProjectFrame(myProject.getName());
+        if (projectFrame != null) {
+            final Rectangle bounds = new Rectangle(windowState.x, windowState.y,
+                    windowState.width, windowState.height);
+            projectFrame.setBounds(bounds);
+            if (windowState.extendedState != -1) {
+                projectFrame.setExtendedState(windowState.extendedState);
+            }
+        }
     }
 
     public void projectClosed() {
@@ -42,5 +68,25 @@ public class WindowDressing extends AbstractProjectComponent {
         final ActionManager actionManager = ActionManager.getInstance();
         return (WindowActionGroup)
                 actionManager.getAction("net.intellij.window.WindowActionGroup");
+    }
+
+    public WindowState getState() {
+        final Frame projectFrame = WindowAction.findProjectFrame(myProject.getName());
+        if (projectFrame != null) {
+            final Rectangle bounds = projectFrame.getBounds();
+            if (windowState == null) {
+                windowState = new WindowState();
+            }
+            windowState.x = bounds.x;
+            windowState.y = bounds.y;
+            windowState.width = bounds.width;
+            windowState.height = bounds.height;
+            windowState.extendedState = projectFrame.getExtendedState();
+        }
+        return windowState;
+    }
+
+    public void loadState(final WindowState windowState) {
+        this.windowState = windowState;
     }
 }
